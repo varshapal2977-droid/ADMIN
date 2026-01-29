374d4909037c0e25e0a8dd7a27d5dad8ecd8d43a
unknown <you@example.com>
2026-01-29 21:01:55 +0530

    added callback functions

--- js/app.js
+++ js/app.js
@@ -4,25 +4,67 @@
 //setInterval(() => {
   
 //}, interval);
-//const timerID = setInterval(() => {console.log("set interval")}, 1000)
-// timeoutID = setTimeout(() => {clearInterval(timerID);console.log("cleared interval")},1000)
-//
+//const timerID = setInterval(() => {console.log("set interval")}, 1000);
+//timeoutID = setTimeout(() => {clearInterval(timerID);console.log("cleared interval")},1000)
 //let num = 1;
-//const timerID = setInterval(() => {
- //if (num == 10) clearInterval(timerID);console.log(num); num++;}, 1000);
- const name = document.querySelector('#name')
- const btn = document.querySelector('.btn')
- const list = document.querySelector('.list')
- btn.addEventListener('click', () => {
-     const li = document.createElement('li')
-     const deleteBtn = document.createElement('button')
-     deleteBtn.innertext = 'Delete'
-     li.innertext = name.value;
-      deleteBtn.addEventListener('click', () => {
-          list.removeChild(li)
-      })
-     list.appendChild(li)
-     li.appendChild(deleteBtn)
-     name.value = ''
+////const  ID = setInterval(() => {
+  //(num == 10) clearInterval(timerID);console.log(num); num++;}, 1000);
+ //const name = document.querySelector('#name')
+ ////const btn = document.querySelector('.btn')
+ //const list = document.querySelector('.list')
+ //if(name.value is ==="")
+ 
+ //setTimeout(()=>{
+   // console.log("time is up");
+//}// ,5000);
+
+//onsole.log("first message");
+////setTimeout(() => {
+   // console.log("second message")
+//}, 2000);
+////console.log("third message");
+//setInterval(()=>{
+    //console.log(" reminder drink water in every 2 seconds");
+//},2000);
+
+//console.log("start the interval....");
+//setInterval(() => {
+    //console.log("repeating messages");
+//},2000);
+
+//console.log("interval has been set");
+
+//function third(){
+//    console.log("third function is rynning(top f the stack");
+//}
+//function second(){
+   /// console.log("second function is running (while calling third)");third();
+//}
+
+//function first(){
+    //console.log("first function is runnig whilecalling he second");second();
+//}
+//console.log("script stareted");
+//first();
+//console.log("script ended");
+//f//unction print(callback){
+    //onsole.log("inside print function");
+    //callback();
+//}   
+//print("callback function is invoked");
+
+function print (num){
+    console.log("inside print function");
     
-  })
+}
+    setTimeout(() => {
+    console.log("inside print function");
+    num()
+
+},2000) ;
+function sample(){
+    console.log("sample function is invoked");
+}
+
+
+
