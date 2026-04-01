aa0c30520235295b225498c666f9e2cd744e3b14
unknown <you@example.com>
2026-04-01 13:47:31 +0530

    Added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h2> This is a NavBar</h2>
+    </div>
+  )
+}
+
+export default Navbar
--- src/components/Samples.jsx
+++ src/components/Samples.jsx
@@ -9,3 +9,24 @@ export default class Samples extends Component {
     )
   }
 }
+class Sample extends Component {
+    constructor(){
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+    increment = () => {
+        this.setState({count: this.state.count + 1})
+    }
+  render() {
+    return (
+      <div>
+        <h1>This is a sample component</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    )
+  }
+}
+
